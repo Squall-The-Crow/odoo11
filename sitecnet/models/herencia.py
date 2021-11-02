e5bd541d8398becd1f183b5b81071952dfd16a67
Squall-The-Crow <mzamora@sitecnet.com>
2021-11-02 15:35:32 -0600

    Update herencia.py

--- sitecnet/models/herencia.py
+++ sitecnet/models/herencia.py
@@ -6,5 +6,9 @@ class Users(models.Model):
     _inherit = "muk_dms.file"
     usuarios = fields.Many2one('sitecnet.usuarios', 'Usuario')
     cliente = fields.Many2one('sitecnet.clientes', 'Cliente')
+    tipo = fields.Selection([('usuario', 'Usuario'),
+                                ('administrador', 'Administrador'),
+                                ('tecnico', 'Tecnico'),
+                                ], string='Tipo de usuario', default='usuario')
 
 Users()
