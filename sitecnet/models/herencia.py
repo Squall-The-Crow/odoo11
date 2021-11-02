9c74b4a71e2b1012d2bab4f7794a148c9157c69c
Squall-The-Crow <mzamora@sitecnet.com>
2021-11-02 15:17:30 -0600

    Prueba1
    Pruieba4

--- sitecnet/models/herencia.py
+++ sitecnet/models/herencia.py
@@ -5,5 +5,6 @@ from odoo import fields, models, api
 class Users(models.Model):
     _inherit = "muk_dms.file"
     usuarios = fields.Many2one('sitecnet.usuarios', 'Usuario')
+    cliente = fields.Many2one('sitecnet.clientes', 'Cliente')
 
 Users()
