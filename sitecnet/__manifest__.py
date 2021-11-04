838341357a0722aecf39128bd95bcaa3e8cc62da
Squall-The-Crow <mzamora@sitecnet.com>
2021-11-04 12:33:26 -0600

    Update __manifest__.py

--- sitecnet/__manifest__.py
+++ sitecnet/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '2.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'contacts', 'decimal_precision', 'web_google_maps', 'muk_dms_file', 'res.users'],
+    'depends': ['base', 'contacts', 'decimal_precision', 'web_google_maps', 'muk_dms_file'],
 
     # always loaded
     'data': [
