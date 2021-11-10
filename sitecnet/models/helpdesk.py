d0290a021f216efbfab14cef35d5c9ac66afee1c
Squall-The-Crow <mzamora@sitecnet.com>
2021-11-09 21:44:28 -0600

    Update helpdesk.py

--- sitecnet/models/helpdesk.py
+++ sitecnet/models/helpdesk.py
@@ -14,9 +14,15 @@ class helpdesk(models.Model):
                                    ('procedimiento', 'Solicitud de procedimiento / configuracion'),
                                     ('solicitud de conferencia', 'Solicitud de conferencia / capacitacion'),
                                    ], string='Tipo de soporte', default='consulta')
+<<<<<<< HEAD
     empresa = fields.Many2one('res.partner', string='Cliente')#domain=[('is_company', '=', True)]
     usuario = fields.Many2one('res.partner', string='Usuario con problemas') #domain= [('partner_ids','in', empresa.id)]
     equipo = fields.Many2one('sitecnet.equipos', 'Equipo con problemas') #poner domain domain=[('partner.id', '=', usuario.id)]
+=======
+    empresa = fields.Many2one('res.partner', string='Cliente', domain=[('is_company', '=', True)])
+    usuario = fields.Many2one('res.partner', string='Usuario con problemas', domain= [('partner_ids','in', empresa.id)])
+    equipo = fields.Many2one('sitecnet.equipos', 'Equipo con problemas',  domain=[('partner.id', '=', usuario.id)]) #poner domain
+>>>>>>> parent of c4dae1e (Update helpdesk.py)
     conexion = fields.Char('Conexion Remota')
     actividades = fields.One2many('sitecnet.actividades', 'reporte', string='Actividades', copy=True, auto_join=True)
     calificacion = fields.Selection([('bueno', 'Bueno'),
